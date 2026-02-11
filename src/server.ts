f06c0f73826bdb206921e8ae203c2358fba19139
Touiiix <leolelievre2006@gmail.com>
2026-02-11 19:07:07 +0100

    point_5

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
+import express, {Request, Response} from 'express';
+
 function greet(name: string): string {
     return `Hello, ${name}!`;
 }
 
 let message = greet("Pepito");
 console.log(message);
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Bienvenue sur mon serveur API')
+})
+app.listen(port, () => {
+    console.log(`Serveur lancé sur http://localhost:${port}`);
+});
