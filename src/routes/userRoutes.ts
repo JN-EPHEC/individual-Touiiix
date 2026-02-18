88ddfcb50a956357000ea32f91925ef08b7d7526
Touiiix <leolelievre2006@gmail.com>
2026-02-18 18:27:57 +0100

    homework_1_modification POST/ et DELETE/:id gestion d'erreur

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,6 @@
 import express, {Router, Request, Response} from 'express';
 import User from '../models/User';
+import { error } from 'console';
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
@@ -8,18 +9,31 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 router.post('/', async (req: Request, res: Response) => {
+  try{
   const {Prenom, Nom} = req.body;
+  if(!Prenom || Prenom.trim() === ""){
+    return res.status(400).send({error: "Il faut un Prénom"});
+  }
   const newUser = await User.create({Prenom, Nom});
-  res.status(201).json(newUser);
-  
+  return res.status(201).send(`Utilisateur créé avec succès : ${newUser}`);
+  }catch(error){
+    return res.status(500).send({error: "erreur du serveur"})
+  }
 });
 
 router.delete('/:id', async (req: Request, res: Response) => {
+  try{
   const {id} = req.params;
 
-  await User.destroy({
+  const supprime = await User.destroy({
     where: {id},
   });
-  res.json({message: 'Utilisateur supprimé'});
+  if (supprime === 0){
+    return res.status(404).send({error: "l'utilisateur n'existe pas"});
+  }
+  res.send(`Utilisateur supprimé ${id}`);
+}catch(error){
+  return res.status(500).send({error: "erreur du serveur"})
+}
 })
 export default router;
