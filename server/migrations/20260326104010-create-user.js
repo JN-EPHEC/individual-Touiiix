e728f2c6aed8a57757506a965def5b678b610264
Touiiix <leolelievre2006@gmail.com>
2026-03-26 13:33:49 +0100

    TP5_3.2

---
+++ server/migrations/20260326104010-create-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      Prenom: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      Nom: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+   },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  }
+};
